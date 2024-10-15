f7beeca6f5bbbdf4ec265b6fe67d244e7cf068fb
Athira Thilakan <athilakan@wisilica.com>
2024-10-15 12:09:38 +0530

    Initial Commit

---
+++ Ust_TestDemoUITests/Ust_TestDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ust_TestDemoUITestsLaunchTests.swift
+//  Ust_TestDemoUITests
+//
+//  Created by Athira Thilakan on 15/10/24.
+//
+
+import XCTest
+
+final class Ust_TestDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
